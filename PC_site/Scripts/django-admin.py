b120540f300438e7d33ba48c8f7dd37f03a1f778
Ramil245048 <ramil.durov.96@gmail.com>
2018-12-12 17:57:31 +0300

    Initial commit

---
+++ PC_site/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Ramil\PycharmProjects\Site_Test_041218\PC_site\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
